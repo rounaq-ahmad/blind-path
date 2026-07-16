d332518ac2289ee9e3228e2597cf71fc9912488e
Rounaq <rounaq.ahmad@ringcentral.com>
2026-07-16 10:00:04 +0530

    Fix config plugin: use line-filter instead of regex for enableBundleCompression

--- withFixBuildGradle.js
+++ withFixBuildGradle.js
@@ -3,7 +3,9 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 module.exports = function withFixBuildGradle(config) {
   return withAppBuildGradle(config, (mod) => {
     mod.modResults.contents = mod.modResults.contents
-      .replace(/[ \t]*enableBundleCompression\s*=\s*(true|false)\r?\n?/g, '');
+      .split('\n')
+      .filter(line => !line.includes('enableBundleCompression'))
+      .join('\n');
     return mod;
   });
 };
